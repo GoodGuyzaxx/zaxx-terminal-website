e5c7eb762ddb893fc51533a6f8cc466a75a9eb0e
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2025-01-04 20:49:42 +0900

    Fix : Add projects to help and alias project

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -2,7 +2,7 @@ import { formatProjectList } from "./formatters";
 import { projects } from "./projects";
 
 export const commands = {
-  help: 'Available commands: help, about, contact, hardware, clear',
+  help: 'Available commands: help, about, contact, hardware, projects, clear',
   about: 'Hello, I am Dewantara, a final semester student majoring in Computer Science, with experience in Full Stuck Mobile Developments and with a high learning intention.',
   hardware: `Macbook Pro 2017 : 
   - Intel Core i5 dual-core 2,3 GHz
@@ -14,5 +14,6 @@ export const commands = {
   - Instagram: https://instagram.com/dewntraa
   - Email : goodguyzaxx@gmail.com`,
   projects: formatProjectList(),
+  project: formatProjectList(),
   clear: '',
 };
